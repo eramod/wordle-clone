584ada69dd5056fe77a56cfe22bfb69c81f3e3c8
eramod <dianne.eramo@gmail.com>
2022-07-22 19:10:01 -0700

    Start work on entering and displaying guesses

--- app/components/keyboard.ts
+++ app/components/keyboard.ts
@@ -1,6 +1,8 @@
-import { action } from '@ember/object';
+import { action, get } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import Component from '@glimmer/component';
+import { TrackedArray } from 'tracked-built-ins';
+import { WORDLIST } from '../utils/word-list';
 
 interface KeyboardArgs {}
 
@@ -10,15 +12,37 @@ export default class Keyboard extends Component<KeyboardArgs> {
    */
   @tracked currentGuess = '';
 
+  guesses: TrackedArray<string[]> = new TrackedArray([]);
+
+  maxGuesses = 6;
+
+  // TODO: Make this property update on a daily basis.
+  // WORDLIST[Math.floor(Math.random() * WORDLIST.length)];
+  wordOfTheDay: string = 'hello'; // Predictable for development purposes
+
+  /**
+   * The official Wordle list of possible words.
+   */
+  wordList: string[] = WORDLIST.split(/\r?\n/);
+
   /**
    * The layout of keys to display on the game's virtual keyboard, with each
    * nested array representing a row.
    */
+  // TODO: Replace enter and backspace with icons.
   keyLayout: string[][] = [
     ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
     ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
     ['Enter', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Backspace'],
   ];
+
+  get guessCount() {
+    return this.guesses.length;
+  }
+
+  get isGuessInWordList(): boolean {
+    return this.wordList.includes(this.currentGuess);
+  }
 
   /**
    * Adds the letter clicked on in the game's virtual keyboard to the
@@ -55,8 +79,24 @@ export default class Keyboard extends Component<KeyboardArgs> {
   handleKeyInput(key: string): void {
     switch (key) {
       case 'Enter':
-        // TODO: Submit the guess once game play is implemented
-        console.log(`The content is ${this.currentGuess}`);
+        if (this.currentGuess.length < 5) {
+          alert('Not enough letters');
+        } else if (!this.isGuessInWordList) {
+          alert(`${this.currentGuess} not in word list`);
+          this.currentGuess = '';
+        } else if (
+          this.isGuessInWordList &&
+          this.guesses.length < this.maxGuesses
+        ) {
+          // TODO: Check each letter against the word to see if it's in the word and whether it's in the correct position.
+          this.guesses.push(this.currentGuess.split(''));
+          // debugger;
+          this.currentGuess = '';
+
+          console.log('Guesses: ', this.guesses);
+        }
+
+        this.currentGuess = '';
         break;
 
       case 'Backspace':
@@ -67,7 +107,9 @@ export default class Keyboard extends Component<KeyboardArgs> {
         break;
 
       default:
-        this.currentGuess += key;
+        if (this.currentGuess.length < 5) {
+          this.currentGuess += key;
+        }
     }
   }
 }
--- project-plan.md
+++ project-plan.md
@@ -29,6 +29,13 @@ Component Design
   - Keyboard
     - Letter - color, action to change the color (can probably reuse)
 
+Data definitions
+  [X] Set up word list of allowed words. Copy from the official Wordle game.
+  [X] Set up a tracked array property of all the guesses
+  [X] Set up a max guesses property (6)
+  [X] Word of the day, chosen at "random" daily from the master list
+  [X] Guess count
+
 To Do List:
 [X] Install typescript
 [X] Create keyboard that recognizes letters entered and can update a `currentGuess` property in the UI
@@ -36,7 +43,19 @@ To Do List:
 [X] CSS for keyboard
 [X] Handle click event on letter keys so user can use the game's keyboard to enter letters
 [X] Implement `backspace` for both the click event on the game's keyboard and the keyup event from typing on the real keyboard. Take a substring of the current guess that does not include the last letter.
- 
+[X] UI: Add a representation of the user's guesses, with the current one being editable
+  [X] Start with a hard coded list of guesses just so you can show it in the UI
+[ ] CSS for guesses display
+[ ] Implement game logic - see game definition when you're ready to add more detail to this section
+[ ] Add enter key functionality
+  [ ] If guess matches word of the day, alert user you win!
+  [ ] If guess is not in the word list, alert user "Not in word list"
+  [ ] If guess is too short, alert user "Not enough letters"
+  [ ] If guess has already been guessed, alert user "Already guessed"
+[ ] Replace alerts with a "toast" (a pop-up that disappears)
+[ ] Implement end of game functionality. If user guess matches word of the day, alert "You win!". If user runs out of guesses, alert "Better luck next time".
+[ ] CSS for guesses (animation and colors to indicate how letters in a guess compare to the word of the day, green if in the word in the correct spot, yellow if in the word but in the wrong spot, gray if not in the word) and keyboard (colors to match guess colors)
+
 Stretch goals:
 [ ] Implement end game share functionality
 [ ] Implement strict mode
